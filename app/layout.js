57597140578b7665987e7abc0483af19f62f16ef
shahnawaz <shahnawazgull2166@gmail.com>
2025-03-27 17:53:18 +0500

    deploy

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
+import './globals.css'
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="forhtml">
       <link rel="icon" href="assets/favicon.png"/>
       <title>QuickCampaign.io</title>
-      <body>
+      <body className="forbody">
         {children}
       </body>
     </html>
